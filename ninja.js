aff71d2e4c167b71357b664d8f4eb86fcaafaed9
Christopher-Teague <cmteague77@gmail.com>
2022-01-10 12:44:00 -0800

    initial commit

---
+++ ninja.js
@@ -0,0 +1,23 @@
+class Ninja {
+    constructor (name) {
+        this.name = name;
+        this.health = 50;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats() {
+        console.log("Name:" + this.name + " Strength:" + this.strength + " Speed:" +  this.speed +  " Health:" + this.health);
+    }
+
+    drinkSake() {
+        this.health +=10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
